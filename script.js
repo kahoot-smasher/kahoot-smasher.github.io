fc86b918255368dc2d38d610c51fb05bdb567844
BannerBomb <BannerBomb55@gmail.com>
2017-03-02 11:56:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-wif(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
     $(document).ready(function(){
         $("#mobileBody").css("display","block");
         $("#body").css("display","none");
